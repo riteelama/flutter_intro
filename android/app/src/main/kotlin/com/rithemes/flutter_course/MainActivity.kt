9e0353f7066c1378bd4d9fe808b7a5e3c053384e
riteelama <riteelama@gmail.com>
2024-10-13 23:53:10 +0545

    Initial Commit

---
+++ android/app/src/main/kotlin/com/rithemes/flutter_course/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rithemes.flutter_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+// Creates a simple Hello Word text
+// inside the Center widet using material UI.
+
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for al our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}

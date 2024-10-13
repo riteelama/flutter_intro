4c8f03e3e24504a43680a8b3eaff9133c0129ca3
riteelama <riteelama@gmail.com>
2024-10-14 00:50:01 +0545

    Add - Image using AssestImage  according to pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
-// Creates a simple Hello Word text
-// inside the Center widet using material UI.
-
 import 'package:flutter/material.dart';
 
-//The main function is the starting point for al our Flutter apps.
+// The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://prosysthemes.com/wp-content/uploads/2018/01/logo.png'),
-          ),
-        ),
         appBar: AppBar(
           title: Center(child: Text('I am rich')),
           backgroundColor: Colors.blueGrey[900],
           foregroundColor: Colors.white,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/logo.png'),
+          ),
         ),
       ),
     ),
